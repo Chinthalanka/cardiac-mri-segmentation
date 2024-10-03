ca90db4a2bfd9013d236b46a34aa0c6d17fc2b8d
Chinthalanka Wijesinghe <chinthalanka.wijesinghe@gmail.com>
2024-10-03 06:36:14 +0530

    Stack masks inside the dataset

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -13,16 +13,20 @@ def evaluate(net, dataloader, device, amp):
     # iterate over the validation set
     with torch.autocast(device.type if device.type != 'mps' else 'cpu', enabled=amp):
         for batch in tqdm(dataloader, total=num_val_batches, desc='Validation round', unit='batch', leave=False):
-            images, msks_lv, msks_rv, msks_myo = batch['image'], batch['mask_lv'], batch['mask_rv'], batch['mask_myo']
+            # images, msks_lv, msks_rv, msks_myo = batch['image'], batch['mask_lv'], batch['mask_rv'], batch['mask_myo']
+            images, masks = batch['image'], batch['masks']
             images = images.permute(0, 3, 1, 2)
 
             # move images and labels to correct device and type
             images = images.to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
+            mask_true = masks.to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
+            '''
             mask_true = torch.stack(
                 [torch.tensor(msks_lv, dtype=torch.float32),
                  torch.tensor(msks_rv, dtype=torch.float32),
                  torch.tensor(msks_myo, dtype=torch.float32)],
                 dim=0).to(device=device, dtype=torch.float32, memory_format=torch.channels_last).permute(1, 0, 2, 3)
+            '''
 
             # predict the mask
             # mask_pred = net(images).permute(1, 0, 2, 3)
@@ -37,7 +41,8 @@ def evaluate(net, dataloader, device, amp):
                 assert mask_true.min() >= 0 and mask_true.max() < net.n_classes, 'True mask indices should be in [0, n_classes]'
                 # convert to one-hot format
                 mask_true = mask_true.float()
-                mask_pred = mask_pred.float()
+                mask_pred = (F.sigmoid(mask_pred) > 0.5).float()
+                # mask_pred = mask_pred.float()
                 # compute the Dice score, ignoring background
                 dice_score += multiclass_dice_coeff(mask_pred[:, :], mask_true[:, :], reduce_batch_first=False)
 
