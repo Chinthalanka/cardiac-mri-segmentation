224af8890b152771f5a110f4605fc3bdfe28d3f7
Chinthalanka Wijesinghe <chinthalanka.wijesinghe@gmail.com>
2024-11-07 09:40:29 +0530

    Update code for combined mask

--- utils/dice_score.py
+++ utils/dice_score.py
@@ -3,6 +3,9 @@ from torch import Tensor
 
 
 def dice_coeff(input: Tensor, target: Tensor, reduce_batch_first: bool = False, epsilon: float = 1e-6):
+    # Convert target to one-hot encoding
+    target = torch.nn.functional.one_hot(target.long(), num_classes=input.shape[1]).permute(0, 3, 1, 2).float()
+
     # Average of Dice coefficient for all batches, or for a single mask
     assert input.size() == target.size()
     assert input.dim() == 3 or not reduce_batch_first
@@ -19,10 +22,11 @@ def dice_coeff(input: Tensor, target: Tensor, reduce_batch_first: bool = False, 
 
 def multiclass_dice_coeff(input: Tensor, target: Tensor, reduce_batch_first: bool = False, epsilon: float = 1e-6):
     # Average of Dice coefficient for all classes
-    return dice_coeff(input.flatten(0, 1), target.flatten(0, 1), reduce_batch_first, epsilon)
+    # return dice_coeff(input.flatten(0, 1), target.flatten(0, 1), reduce_batch_first, epsilon)
+    return dice_coeff(input, target.flatten(0, 1), reduce_batch_first, epsilon)
 
 
 def dice_loss(input: Tensor, target: Tensor, multiclass: bool = False):
     # Dice loss (objective to minimize) between 0 and 1
     fn = multiclass_dice_coeff if multiclass else dice_coeff
-    return 1 - fn(input, target, reduce_batch_first=True)
+    return 1 - fn(input, target, reduce_batch_first=False)
--- utils/evaluate.py
+++ utils/evaluate.py
@@ -14,12 +14,14 @@ def evaluate(net, dataloader, device, amp):
     with torch.autocast(device.type if device.type != 'mps' else 'cpu', enabled=amp):
         for batch in tqdm(dataloader, total=num_val_batches, desc='Validation round', unit='batch', leave=False):
             # images, msks_lv, msks_rv, msks_myo = batch['image'], batch['mask_lv'], batch['mask_rv'], batch['mask_myo']
-            images, masks = batch['image'], batch['masks']
+            images, masks, masks_all = batch['image'], batch['masks'], batch['masks_all']
             images = images.permute(0, 3, 1, 2)
+            masks_all = masks_all.permute(0, 3, 1, 2)
 
             # move images and labels to correct device and type
             images = images.to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
-            mask_true = masks.to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
+            # mask_true = masks.to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
+            mask_true = masks_all.to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
             '''
             mask_true = torch.stack(
                 [torch.tensor(msks_lv, dtype=torch.float32),
@@ -36,14 +38,15 @@ def evaluate(net, dataloader, device, amp):
                 assert mask_true.min() >= 0 and mask_true.max() <= 1, 'True mask indices should be in [0, 1]'
                 mask_pred = (F.sigmoid(mask_pred) > 0.5).float()
                 # compute the Dice score
-                dice_score += dice_coeff(mask_pred, mask_true, reduce_batch_first=False)
+                dice_score += dice_coeff(mask_pred.squeeze(1), mask_true.float(), reduce_batch_first=False)
             else:
                 assert mask_true.min() >= 0 and mask_true.max() < net.n_classes, 'True mask indices should be in [0, n_classes]'
                 # convert to one-hot format
-                mask_true = mask_true.float()
-                mask_pred = (F.sigmoid(mask_pred) > 0.5).float()
+                mask_true = mask_true.long()
+                # mask_pred = (F.sigmoid(mask_pred) > 0.5).float()
                 # mask_pred = mask_pred.float()
-                # compute the Dice score, ignoring background
+
+                # Compute the Dice score, ignoring background
                 dice_score += multiclass_dice_coeff(mask_pred[:, :], mask_true[:, :], reduce_batch_first=False)
 
     net.train()
