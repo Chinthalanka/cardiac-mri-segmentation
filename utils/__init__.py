54275addbe0551555ff4b9ab6a14233399034d66
Chinthalanka Wijesinghe <chinthalanka.wijesinghe@gmail.com>
2025-01-01 18:11:12 +0530

    Add multi-class dice score function

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
-from utils.dice_score import dice_loss, dice_coeff, weighted_dice_loss
+from utils.dice_score import dice_loss, dice_coeff, weighted_dice_loss, multiclass_dice_coeff
 from utils.evaluate import evaluate
 from utils.utils import EarlyStopping
