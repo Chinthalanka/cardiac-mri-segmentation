69de2436b9b53f88090ed01fef78b3e8d224d8a2
Chinthalanka Wijesinghe <chinthalanka.wijesinghe@gmail.com>
2025-01-01 18:16:15 +0530

    Add transformer-based U-Net

--- experiments/src/__init__.py
+++ experiments/src/__init__.py
@@ -3,6 +3,7 @@ from .residual_unet import ResidualUNet
 from .attention_unet import AttentionUNetV3
 from .feature_pyramid_unet import FeaturePyramidUNet
 from .feedback_resunet import FeedbackResUNet
+from .transUnet import TransformerUNet
 from .train import train_model, weights_init
 from .inference import evaluate_dice_score
 from .visualize import visualize_predictions
